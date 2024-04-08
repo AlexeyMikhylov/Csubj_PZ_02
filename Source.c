968409fef3b1d8523773f695d2c34e537371dac5
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-04-09 02:00:46 +0300

    1, 2, 3, 4

--- Source.c
+++ Source.c
@@ -20,6 +20,32 @@ int run()
     case 0:
         printf("exit");
         return 0;
+    case 1:
+        num1();
+    case 12: //ввод структуры с файла 
+        printf("\n");
+        num1_txtOut();
+        break;
+    case 13: //ввод структуры с клавы, запись структуры в файл
+        printf("\n");
+        num1_txtIn();
+        break;
+    case 2:
+        printf("\n");
+        num2();
+        break;
+    case 3:
+        printf("\n");
+        num3();
+        break;
+    case 4:
+        printf("\n");
+        num4();
+        break;
+    case 5:
+        printf("\n");
+        num5();
+        break;
     case 10:
         printf("\n");
         ex1();
@@ -32,6 +58,18 @@ int run()
         printf("\n");
         ex3();
         break;
+    case 40:
+        printf("\n");
+        ex4();
+        break;
+    case 50:
+        printf("\n");
+        ex5();
+        break;
+    case 60:
+        printf("\n");
+        ex6();
+        break;
     default:
         puts("wrong number");
         break;
@@ -40,6 +78,7 @@ int run()
     run();
 }
 
+//ex 1
 int ex1(void)
 {
     struct
@@ -67,6 +106,7 @@ int ex1(void)
     return 0;
 } 
 
+//ex 2
 int ex2() 
 {
     struct sprav
@@ -99,6 +139,7 @@ int ex2()
     return 0;
 }
 
+//ex 3
 #define SPRAVOCHNIK struct spravochnik
 int ex3()
 {
@@ -141,7 +182,473 @@ int ex3()
     return 0;
 }
 
+//ex 4
+//int const N = 3;
+typedef struct
+{
+    char fio[20];
+    long num;
+} TEL;
+
 int ex4()
+{
+    int N;
+    printf("How many users you want to input?: ");
+    scanf("%d", &N);
+
+    TEL* tel;
+    char fio_tek[20];
+    int i;
+
+    /* Выделение памяти для массива - N элементов */
+    tel = (TEL*)malloc(sizeof(TEL) * N);
+
+    for (i = 0; i < N; i++)
+    {
+        puts("Enter your name: ");
+        scanf("%s", (tel + i)->fio);
+
+        puts("Enter the tlf number: ");
+        scanf("%ld", &(tel + i)->num);
+
+        printf("\n");
+    }
+
+    puts("hoosing a phone by last name ");
+    scanf("%s", fio_tek);
+
+    for (i = 0; i < N; i++, tel++)
+        if (!strcmp(fio_tek, tel->fio)) break;
+    if (i != N)
+        printf("Subscriber info:\n   name: %s\n   phone: %ld\n", fio_tek, tel->num);
+    else
+        puts("Subscriber not found");
+
+    return 0;
+}
+
+//ex 5
+typedef struct {
+    float a;   /* действительная часть */
+    float b;   /* мнимая часть */
+} COMPLEX;
+
+void vvod(COMPLEX*, float, float);
+void sum(COMPLEX*, COMPLEX*, COMPLEX*);
+void out(COMPLEX*);
+
+int ex5()
+{
+    COMPLEX x, y, z;
+
+    vvod(&x, 2.5, 6.7);
+    vvod(&y, 6.89, 8.45);
+
+    puts("Numbers are entered: ");
+    out(&x);
+    out(&y);
+
+    puts("The sum of complex numbers is equal to: ");
+    sum(&x, &y, &z);
+    out(&z);
+}
+
+/* Ввод значений для элементов структуры */
+void vvod(COMPLEX* p, float a, float b)
+{
+    p->a = a;
+    p->b = b;
+    return;
+}
+
+/* Вывод комплексного числа */
+void out(COMPLEX* p)
+{
+    printf("(%.2f,%.2f)\n", (*p).a, (*p).b);
+    return;
+}
+
+/* Вычисление суммы двух комплексных чисел */
+void sum(COMPLEX* p1, COMPLEX* p2, COMPLEX* p3)
+{
+    (*p3).a = (*p1).a + (*p2).a;
+    (*p3).b = (*p1).b + (*p2).b;
+    return;
+}
+
+//ex 6
+typedef struct Model
+{
+    int id;
+    
+    struct
+    {
+        int id;
+        char* name;
+    } make;
+    
+    char* name;
+    
+    unsigned year;
+
+} Model;
+
+int ex6()
+{
+    Model m = { 10, {10, "Acura"}, "CL", 2012 };
+    
+    printf("Model name = %s\n", m.name);
+    printf("Make name = %s\n", m.make.name);
+    printf("year = %d\n", m.year);
+    
+    return 0;
+}
+
+//number 1
+#define students 5
+
+int num1()
+{
+    typedef struct
+    {
+        char surname[20];
+        int group;
+        unsigned int grade[5];
+    } STUDENT;
+
+    int k = 0;
+
+    STUDENT p[students];
+
+    for (int i = 0; i < students; i++)
+    {
+        puts("\nEnter student's surname: ");
+        scanf("%s", &p[i].surname);
+
+        puts("Enter student's group: ");
+        scanf("%ld", &p[i].group);
+
+        puts("Enter student's grade (1..5): ");
+        for (int j = 0; j < 5; j++)
+        {
+            scanf("%ld", &p[i].grade[j]);
+
+            if (p[i].grade[j] > 5)
+                p[i].grade[j] = 5;
+        }
+    }
+
+    puts("\nStudent's with average grade > 4.0:\n");
+    for (int i = 0; i < students; i++)
+    {
+        float all_marks = 0;
+
+        for (int j = 0; j < 5; j++)
+        {
+            all_marks += p[i].grade[j];
+        }
+
+        float average_grade = all_marks / 5;
+
+        if (average_grade > 4.0)
+        {
+            printf("\nSurname: %s \n", p[i].surname);
+            printf("Group: %ld \n", p[i].group);
+            k++;
+        }
+    }
+    if (k == 0)
+    {
+        puts("There are no students with average grade > 4.0");
+    }
+}
+
+int num1_txtOut()
+{
+    typedef struct
+    {
+        char surname[20];
+        int group;
+        unsigned int grade[5];
+    } STUDENT;
+
+    int k = 0;
+
+    STUDENT p[students];
+
+    FILE* fileOut;
+    fileOut = fopen("Students_grades_Output.txt", "r");
+
+    for (int i = 0; i < students; i++)
+    {
+        fscanf(fileOut, "%s %d %d %d %d %d %d", &p[i].surname, &p[i].group, &p[i].grade[0], &p[i].grade[1], &p[i].grade[2], &p[i].grade[3], &p[i].grade[4]);
+    }
+
+    fclose(fileOut);
+
+    puts("\nStudent's with average grade > 4.0:");
+    for (int i = 0; i < students; i++)
+    {
+        float all_marks = 0;
+
+        for (int j = 0; j < 5; j++)
+        {
+            all_marks += p[i].grade[j];
+        }
+
+        float average_grade = all_marks / 5;
+
+        if (average_grade > 4.0)
+        {
+            printf("\nSurname: %s \n", p[i].surname);
+            printf("Group: %ld \n", p[i].group);
+            printf("Average grade: %.2lf \n", average_grade);
+            k++;
+        }
+    }
+    if (k == 0)
+    {
+        puts("There are no students with average grade > 4.0");
+    }
+}
+
+int num1_txtIn()
+{
+    typedef struct
+    {
+        char surname[20];
+        int group;
+        unsigned int grade[5];
+    } STUDENT;
+
+    int k = 0;
+
+    STUDENT p[students];
+
+    FILE* fileIn;
+    fileIn = fopen("Students_grades_Input.txt", "w");
+
+    for (int i = 0; i < students; i++)
+    {
+        fprintf(fileIn, "%d)\n", i+1);
+
+        puts("\nEnter student's surname: ");
+        scanf("%s", &p[i].surname);
+        fprintf(fileIn, "Surname: %s", p[i].surname);
+
+        puts("Enter student's group: ");
+        scanf("%ld", &p[i].group);
+        fprintf(fileIn, "\nGroup: %d", p[i].group);
+
+        puts("Enter student's grade (1..5): ");
+
+        fprintf(fileIn, "\nGrades:\n\t");
+        for (int j = 0; j < 5; j++)
+        {
+            scanf("%ud", &p[i].grade[j]);
+
+            if (p[i].grade[j] > 5)
+                p[i].grade[j] = 5;
+            fprintf(fileIn, "  %u", p[i].grade[j]);
+        }
+
+        fprintf(fileIn, "\n\n");
+    }
+    fclose(fileIn);
+
+    puts("\nStudent's with average grade > 4.0:");
+    for (int i = 0; i < students; i++)
+    {
+        float all_marks = 0;
+
+        for (int j = 0; j < 5; j++)
+        {
+            all_marks += p[i].grade[j];
+        }
+
+        float average_grade = all_marks / 5;
+
+        if (average_grade > 4.0)
+        {
+            printf("\nSurname: %s \n", p[i].surname);
+            printf("Group: %ld \n", p[i].group);
+            printf("Average grade: %.2lf \n", average_grade);
+            k++;
+        }
+    }
+    if (k == 0)
+    {
+        puts("There are no students with average grade > 4.0");
+    }
+}
+
+//number 2
+int num2()
+{
+    typedef struct
+    {
+        char surname[20];
+        int group;
+        int grade[5];
+    } STUDENT;
+
+    int k = 0;
+    
+    STUDENT p[10];
+    
+    for (int i = 0; i < students; i++)
+    {
+        puts("\nEnter student's surname: ");
+        scanf("%s", &p[i].surname);
+
+        puts("Enter student's group: ");
+        scanf("%ld", &p[i].group);
+
+        puts("Enter student's grade (1..5): ");
+        for (int j = 0; j < 5; j++)
+        {
+            scanf("%ld", &p[i].grade[j]);
+        }
+    }
+
+    puts("\nStudents that have only 4 and 5 grades: ");
+    for (int i = 0; i < students; i++)
+    {
+        float marks = 0;
+        for (int j = 0; j < 5; j++)
+        {
+            if (p[i].grade[j] == 4 || p[i].grade[j] == 5)
+            {
+                marks == 0;
+            }
+            else
+            {
+                marks++;
+            }
+        }
+        if (marks == 0)
+        {
+            printf("\nSurname: %s \n", p[i].surname);
+            printf("Group: %ld \n", p[i].group);
+            k++;
+        }
+    }
+    if (k == 0)
+    {
+        puts("There are no students that have only 4 and 5 grades");
+    }
+}
+
+//number 3
+int num3()
+{
+    typedef struct
+    {
+        char surname[20];
+        int group;
+        int grade[5];
+    } STUDENT;
+    
+    int k = 0;
+    
+    STUDENT p[10];
+
+    for (int i = 0; i < students; i++)
+    {
+        puts("\nEnter student's surname: ");
+        scanf("%s", &p[i].surname);
+
+        puts("Enter student's group: ");
+        scanf("%ld", &p[i].group);
+
+        puts("Enter student's grade (1..5): ");
+        for (int j = 0; j < 5; j++)
+        {
+            scanf("%ld", &p[i].grade[j]);
+        }
+    }
+    
+    puts("\nStudents that have at least one F grade (2): ");
+    for (int i = 0; i < students; i++)
+    {
+        int fGrade = 0;
+        for (int j = 0; j < 5; j++)
+        {
+            if (p[i].grade[j] == 2)
+            {
+                fGrade++;
+            }
+        }
+        if (fGrade != 0)
+        {
+            printf("\nSurname: %s \n", p[i].surname);
+            printf("Group: %ld \n", p[i].group);
+            k++;
+        }
+    }
+    if (k == 0)
+    {
+        puts("There are no students that have at least one F grade (2)");
+    }
+}
+
+//numbers 4-5
+
+typedef struct
+{
+    char destination[30];
+    int number;
+    char airplane[20];
+} AEROFLOT;
+
+#define FLIGHT 7
+
+//number 4
+int num4()
+{
+    AEROFLOT Flight[FLIGHT];
+    char ur_fl_d[30];
+    int k = 0;
+
+    for (int i = 0; i < FLIGHT; i++)
+    {
+        puts("\nEnter flight destination: ");
+        scanf("%s", &Flight[i].destination);
+
+        puts("Enter flight number: ");
+        scanf("%ld", &Flight[i].number);
+
+        puts("Enter type of airplane: ");
+        scanf("%s", &Flight[i].airplane);
+    }
+
+    puts("Enter destination to find available flights: ");
+    scanf("%s", &ur_fl_d);
+
+    for (int i = 0; i < FLIGHT; i++)
+    {
+        char* f1 = Flight[i].destination;
+        char* f2 = ur_fl_d;
+
+        if (strcmp(f1, f2) == 0)
+        {
+            puts("\nThe flight number: ");
+            printf("%d", Flight[i].number);
+
+            puts("\nThe type of airplane: ");
+            printf("%s", Flight[i].airplane);
+
+            k++;
+        }
+    }
+    if (k == 0)
+    {
+        puts("There are no flights..");
+    }
+}
+
+//number 5
+int num5()
 {
 
 }
